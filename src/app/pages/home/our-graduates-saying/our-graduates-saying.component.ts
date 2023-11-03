c581b51367c1914736f473d5f80d6491b6f51204
root <root@DESKTOP-DJ0N8EH>
2023-11-03 17:23:29 +0500

    home page changes

---
+++ src/app/pages/home/our-graduates-saying/our-graduates-saying.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-graduates-saying',
+  templateUrl: './our-graduates-saying.component.html',
+  styleUrls: ['./our-graduates-saying.component.scss']
+})
+export class OurGraduatesSayingComponent {
+
+}
---
+++ src/app/pages/home/our-graduates/our-graduates.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-graduates',
+  templateUrl: './our-graduates.component.html',
+  styleUrls: ['./our-graduates.component.scss']
+})
+export class OurGraduatesComponent {
+
+}
