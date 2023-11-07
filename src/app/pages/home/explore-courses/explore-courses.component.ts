df112e9cf329a3136b12bfcadce71c5c1df7cd48
Faizanaalogics <faizanzakir.aalogics@gmail.com>
2023-11-07 21:04:29 +0500

    Blog dynamic with api and inner page

--- src/app/pages/home/explore-courses/explore-courses.component.ts
+++ src/app/pages/home/explore-courses/explore-courses.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare var $: any; // Declare jQuery
 
 @Component({
   selector: 'app-explore-courses',
   templateUrl: './explore-courses.component.html',
   styleUrls: ['./explore-courses.component.scss']
 })
-export class ExploreCoursesComponent {
-
+export class ExploreCoursesComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    $('.tab-link').on('click', function(this: HTMLElement) { // Add the type annotation
+      const tabId = $(this).data('tab');
+      $('.tab-content').hide();
+      $('#' + tabId).show();
+    });
+  }
 }
