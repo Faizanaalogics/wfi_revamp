ef0a099afb1ed13aa544e0fb20451e23ca7c63bc
Faizanaalogics <faizanzakir.aalogics@gmail.com>
2023-11-08 20:13:51 +0500

    about component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PostDetailPageComponent } from './post-detail/post-detail-page/post-detail-page.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 const routes: Routes = [
   {path : '', component : HomeComponent},
+  {path : 'about', component : AboutUsComponent},
   {path: ':cat/:identifier', component: PostDetailPageComponent },
 ];
 
