a830bc6d303b8564f2415c92c281c3a90306f3d9
root <root@DESKTOP-DJ0N8EH>
2023-11-17 12:59:00 +0500

    ui ux page changes

---
+++ src/app/pages/ui-ux-design/next-ux-design/next-ux-design.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-next-ux-design',
+  templateUrl: './next-ux-design.component.html',
+  styleUrls: ['./next-ux-design.component.scss']
+})
+export class NextUxDesignComponent {
+
+}
